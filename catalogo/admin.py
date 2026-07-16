5e15372c62db67a046f1ec13baa33f8d0abec111
cristian <cristian.tenorio@uisek.edu.ec>
2026-07-16 18:46:13 -0500

    Agregar modelos Artista y Album con relación uno a muchos

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Artista, Album
 
-# Register your models here.
+admin.site.register(Artista)
+admin.site.register(Album)
--- catalogo/models.py
+++ catalogo/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Artista(models.Model):
+    nombre = models.CharField(max_length=200)
+    biografia = models.TextField(blank=True)
+    genero_musical = models.CharField(max_length=100)
+    fecha_formacion = models.DateField()
+
+    def __str__(self):
+        return self.nombre
+
+
+class Album(models.Model):
+    artista = models.ForeignKey(Artista, related_name='albumes', on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=200)
+    fecha_lanzamiento = models.DateField()
+    numero_canciones = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.titulo} - {self.artista.nombre}"
+    
