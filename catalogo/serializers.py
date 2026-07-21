823218b5f21a650a389b6bc4c4ab1f775d85aa02
cristian <cristian.tenorio@uisek.edu.ec>
2026-07-20 21:02:07 -0500

    Agregar artista_nombre de solo lectura en AlbumSerializer

--- catalogo/serializers.py
+++ catalogo/serializers.py
@@ -3,9 +3,11 @@ from .models import Artista, Album
 
 
 class AlbumSerializer(serializers.ModelSerializer):
+    artista_nombre = serializers.CharField(source='artista.nombre', read_only=True)
+
     class Meta:
         model = Album
-        fields = ['id', 'titulo', 'fecha_lanzamiento', 'numero_canciones', 'artista']
+        fields = ['id', 'titulo', 'fecha_lanzamiento', 'numero_canciones', 'artista', 'artista_nombre']
 
 
 class ArtistaSerializer(serializers.ModelSerializer):
@@ -14,3 +16,4 @@ class ArtistaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Artista
         fields = ['id', 'nombre', 'biografia', 'genero_musical', 'fecha_formacion', 'foto', 'albumes']
+        
